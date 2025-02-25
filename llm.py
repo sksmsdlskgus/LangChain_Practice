bee7804a63b679e9e3110938030ed342a55df4b4
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2025-02-25 15:36:50 +0900

    api 개인키 환경변수 개선

--- llm.py
+++
@@ -1,9 +0,0 @@
-from langchain_ollama import ChatOllama
-
-# LLM 모델 초기화
-llm = ChatOllama(model="llama3.1-instruct-8b:latest")
-
-# 모델을 통해 응답을 받는 함수
-def get_response(user_message: str):
-    response = llm.generate([user_message])
-    return response
